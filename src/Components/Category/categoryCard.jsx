4671c05fb9b708c6cd8d6cc01351a2883aefe245
nuredin05 <mada4everlo@gmail.com>
2025-12-17 14:32:40 +0300

    card catagory

--- src/Components/Category/categoryCard.jsx
+++ src/Components/Category/categoryCard.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import classes from './category.module.css'
 import { Link } from 'react-router-dom';
 
-const CategoryCard = ({data}) => {
+const categoryCard = ({ data }) => {
   return (
     <div className={classes.category}>
-        <Link to={`/category/${data.name}`}>
-            <span>
-                <h2>
-                    {data?.title}
-                </h2>
-                <img src={data?.imgLink} alt=""/>
-                <p>shop now</p>
-            </span>
-        </Link>
+      <Link to={`/category/${data.name}`}>
+        <span>
+          <h2>
+            {data?.title}
+          </h2>
+          <img src={data?.imgLink} alt="" />
+          <p>shop now</p>
+        </span>
+      </Link>
     </div>
   )
 }
 
-export default CategoryCard
+export default categoryCard;
