48511d573cbc32068b2eae384710d783d1b71b7d
nuredin05 <mada4everlo@gmail.com>
2025-12-17 14:05:26 +0300

    update main jd

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { DataProvider } from "./Components/dataProvider/dataProvider.jsx";
-import { initialstate, reducer } from "./utils/reducer.js";
+import { initialstate, reducer } from "./Utils/reducer.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
